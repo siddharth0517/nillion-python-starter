02afbaa170cd653c6d8f771de28476d6becfea37
siddharth0517 <siddharthjaiswalvns123@gmail.com>
2024-07-01 08:39:13 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,24 +1,23 @@
 from nada_dsl import *
 
+def gcd(a, b):
+    zero = SecretInteger(0)
+    
+    while not (b == zero):
+        temp = b
+        b = a % b
+        a = temp
+        
+    return a
 
 def nada_main():
-    # Define the party
     party1 = Party(name="Party1")
     
-    # Define secret integer inputs for the party
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
-    
-    # Perform various computations
-    addition = my_int1 + my_int2
-    subtraction = my_int1 - my_int2
-    multiplication = my_int1 * my_int2
-    division = my_int1 / my_int2
-    
-    # Return the results as outputs
-    return [
-        Output(addition, "addition_result", party1),
-        Output(subtraction, "subtraction_result", party1),
-        Output(multiplication, "multiplication_result", party1),
-        Output(division, "division_result", party1)
-    ]
+
+    # Compute the GCD of the two input integers
+    gcd_result = gcd(my_int1, my_int2)
+
+    # Return the GCD as an output
+    return [Output(gcd_result, "gcd_output", party1)]
